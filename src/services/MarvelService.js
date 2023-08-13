9940a83759755eec520b5ccb2c4492629248acb2
Nadezhda <president.ru@mail.ru>
2023-08-13 13:27:04 +0300

    first commit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,32 @@
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=9be1231460df58125cd413392df451fa'
+    getResource = async(url) => {
+        let res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Couldn't fetch ${url}, status ${res.status}`);
+        }
+        return await res.json();
+    }
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+    getCharacter = async (id) => {
+        const res =  await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1]    
+        }
+    }
+}
+
+export default MarvelService;
